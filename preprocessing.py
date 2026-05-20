611f85009383b09489ff989c91bb545dca449d56
Mataly499 <mataly499@gmail.com>
2026-05-20 11:15:27 -0400

    the new updated preprocessing pipeline using OCR

--- preprocessing.py
+++ preprocessing.py
@@ -1,10 +1,10 @@
 # importing libraries used to extract text and regex text cleaning
 import re
 
-# loads extracted txt resume file
+# this loads extracted txt resume file
 def load_txt_file(filepath):
 
-    # open txt file with utf-8 encoding
+    # opening txt file with utf-8 encoding
     with open(filepath, "r", encoding="utf-8") as file:
 
         # read all text from file
@@ -20,13 +20,19 @@ def clean_text_for_sbert(raw_text):
     # lowercase text for consistent comparisons
     text = raw_text.lower()
 
-    text = text.replace("removed", "")
 
+    text = re.sub(r'\bremoved\b', ' ', text)
+
+    # my OCR cleanup
+    # fixes broken spacing/newlines/tabs
+    text = re.sub(r'[\r\n\t]+', ' ', text)
+
+    text = re.sub(r'_+', ' ', text)
+    
     # normalize whitespace/newlines into single spaces
     text = re.sub(r'\s+', ' ', text).strip()
 
     # lightly remove weird special characters
-    # while preserving semantic meaning
     text = re.sub(r'[^\w\s.,()-]', ' ', text)
 
     # normalize spaces again after regex cleanup
@@ -42,8 +48,8 @@ def process_resume(txt_filepath):
     # load extracted txt file
     raw_text = load_txt_file(txt_filepath)
 
-    # clean text for embeddings
+    
     cleaned_text = clean_text_for_sbert(raw_text)
 
-    # return one cleaned string
+    
     return cleaned_text
